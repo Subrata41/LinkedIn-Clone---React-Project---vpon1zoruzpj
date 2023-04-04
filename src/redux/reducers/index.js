a759c06861cea5611f7ba17efde546ab9b1c7d55
Subrata Roy <113381826+Subrata41@users.noreply.github.com>
2023-04-05 00:22:31 +0530

    Add files via upload

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,1 +1,10 @@
+import { combineReducers } from "redux";
+import articlesReducer from "./articleReducer";
+import userReducer from "./userReducer";
 
+const rootReducer = combineReducers({
+  userState: userReducer,
+  articleState: articlesReducer,
+});
+
+export default rootReducer;
