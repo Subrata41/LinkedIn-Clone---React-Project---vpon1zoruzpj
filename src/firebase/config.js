69901eba60bbabb870cc91769094ac7d126336f9
Subrata Roy <113381826+Subrata41@users.noreply.github.com>
2023-04-12 15:35:28 +0530

    Add files via upload

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDcjXRFLslb4RCKkIH2m3rUHVJGuFyGZKw",
+  authDomain: "linkedin-clone-5a055.firebaseapp.com",
+  databaseURL: "https://linkedin-clone-5a055-default-rtdb.firebaseio.com",
+  projectId: "linkedin-clone-5a055",
+  storageBucket: "linkedin-clone-5a055.appspot.com",
+  messagingSenderId: "452191731768",
+  appId: "1:452191731768:web:3a86bf0e3e8b8d39cca6cd",
+};
+
+export default firebaseConfig;
